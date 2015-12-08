9d8b9070111a2b4b515275e4a6a97f92eb9c9b76
Lu Li <lu.li@liferay.com>
2015-12-08 18:19:08 +0800

    IDE-2143 Swtbot TreePageObject

---
+++ tools/tests/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/ProjectBuildAction.java
@@ -0,0 +1,18 @@
+
+package com.liferay.ide.project.ui.tests;
+
+import com.liferay.ide.ui.tests.UIBase;
+
+public interface ProjectBuildAction extends UIBase
+{
+
+    public final String CONTEXT_MENU_LIFERAY = "Liferay";
+    public final String MENU_SDK = "SDK";
+    public final String MENU_MAVEN = "Maven";
+    public final String CONTEXT_MENU_BUILD_SERVICE = "Build Services";
+    public final String CONTEXT_MENU_BUILD_WSDD = "Build WSDD";
+    public final String MENU_ALL = "all";
+    public final String DELETE_FROM_DISK = "Delete project contents on disk (cannot be undone)";
+    public final String DELETE_RESOURCE = "Delete Resources";
+
+}

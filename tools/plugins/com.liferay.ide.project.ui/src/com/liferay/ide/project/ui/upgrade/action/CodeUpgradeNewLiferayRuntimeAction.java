d672a0248bcc76acdccb8533a152b2ab4dae6944
Joye Luo <joye.luo@liferay.com>
2016-08-12 15:26:10 +0800

    IDE-2848 fix choose liferay server name bug in upgradetool

--- tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/action/CodeUpgradeNewLiferayRuntimeAction.java
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/action/CodeUpgradeNewLiferayRuntimeAction.java
@@ -1,5 +1,4 @@
 /*******************************************************************************
-/*******************************************************************************
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it under
@@ -17,26 +16,29 @@
 package com.liferay.ide.project.ui.upgrade.action;
 
 import org.eclipse.sapphire.ui.Presentation;
+import org.eclipse.sapphire.ui.SapphireActionHandler;
+import org.eclipse.wst.server.ui.ServerUIUtil;
 
 import com.liferay.ide.project.core.upgrade.CodeUpgradeOp;
-import com.liferay.ide.project.ui.action.NewLiferayRuntimeAction;
+import com.liferay.ide.ui.util.UIUtil;
 
 /**
- * @author Terry Jia
+ * @author Joye Luo
  */
-public class CodeUpgradeNewLiferayRuntimeAction extends NewLiferayRuntimeAction
+public class CodeUpgradeNewLiferayRuntimeAction extends SapphireActionHandler
 {
 
     @Override
     protected Object run( Presentation context )
     {
-        Object status = super.run( context );
+        ServerUIUtil.showNewServerWizard( UIUtil.getActiveShell(), "liferay.bundle", null, "com.liferay." );
 
         CodeUpgradeOp op = context.part().getModelElement().nearest( CodeUpgradeOp.class );
 
         op.property( CodeUpgradeOp.PROP_LIFERAY_SERVER_NAME ).refresh();
 
-        return status;
+        return null;
     }
 
 }
+

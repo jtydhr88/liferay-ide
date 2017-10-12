07c578f0fbd2dd819251e329688bfd1dabba97af
Simon Jiang <simon.jiang@liferay.com>
2017-10-12 17:14:07 +0800

    IDE-3340 Liferay IDE check server status error for Liferay Portal CE GA4 for JBoss EAP

--- ui-tests/tools/com.liferay.ide.ui.server.tests/src/com/liferay/ide/ui/server/tests/ServerWildflyTests.java
+++ ui-tests/tools/com.liferay.ide.ui.server.tests/src/com/liferay/ide/ui/server/tests/ServerWildflyTests.java
@@ -15,16 +15,217 @@
 package com.liferay.ide.ui.server.tests;
 
 import com.liferay.ide.ui.liferay.SwtbotBase;
+import com.liferay.ide.ui.liferay.page.editor.ServerEditor;
 
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IPath;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author Terry Jia
+ * @author Simon Jiang
  */
 public class ServerWildflyTests extends SwtbotBase {
 
+	@BeforeClass
+	public static void prepareServer() throws IOException {
+		envAction.unzipWildflyServer();
+
+		envAction.prepareGeoFile();
+
+		envAction.preparePortalWildflyExtFile();
+
+		envAction.prepareWildflyPortalSetupWizardFile();
+
+		String serverName = "Liferay Wildfly 7-initialization";
+
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.openServerRuntimeEnvironmentsDialogTry();
+
+		dialogAction.openNewRuntimeWizard();
+
+		wizardAction.prepareLiferay7RuntimeType();
+
+		wizardAction.next();
+
+		IPath serverDir = envAction.getLiferayWildflyServerDir();
+
+		IPath fullServerDir = serverDir.append(envAction.getLiferayWildflyPluginServerName());
+
+		wizardAction.prepareLiferay7RuntimeInfo(serverName, fullServerDir.toOSString());
+
+		wizardAction.finish();
+
+		dialogAction.confirmPreferences();
+
+		wizardAction.openNewLiferayServerWizard();
+
+		wizardAction.prepareNewServer(serverName);
+
+		wizardAction.finish();
+
+		viewAction.showServersView();
+
+		// String serverStoppedLabel = serverName + " [Stopped]";
+
+		//
+
+		// viewAction.serverStart(serverStoppedLabel);
+
+		//
+
+		// viewAction.serverStartWait();
+
+		//
+
+		// String serverStartedLabel = serverName + " [Started]";
+
+		//
+
+		// viewAction.openLiferayPortalHome(serverStartedLabel);
+
+		//
+
+		// viewAction.serverStop(serverStartedLabel);
+
+		//
+
+		// viewAction.serverStopWait();
+
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.deleteRuntimeTryConfirm(serverName);
+
+		dialogAction.confirmPreferences();
+	}
+
 	@Test
-	public void addLiferayRuntime() {
+	public void addLiferay7RuntimeFromPreferences() {
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.openNewRuntimeWizard();
+
+		wizardAction.prepareLiferay7RuntimeType();
+
+		wizardAction.next();
+
+		String runtimeName = "Liferay 7-wildfly-runtime";
+
+		wizardAction.prepareLiferay7RuntimeInfo(runtimeName, envAction.getLiferayWildflyServerFullDir().toOSString());
+
+		wizardAction.finish();
+
+		dialogAction.confirmPreferences();
+
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.deleteRuntimeTryConfirm(runtimeName);
+
+		dialogAction.confirmPreferences();
+	}
+
+	@Test
+	public void addLiferayWildflyServerFromMenu() {
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.openNewRuntimeWizard();
+
+		wizardAction.prepareLiferay7RuntimeType();
+
+		wizardAction.next();
+
+		String runtimeName = "Liferay 7-wildfly-server";
+
+		wizardAction.prepareLiferay7RuntimeInfo(runtimeName, envAction.getLiferayWildflyServerFullDir().toOSString());
+
+		wizardAction.finish();
+
+		dialogAction.confirmPreferences();
+
+		wizardAction.openNewLiferayServerWizard();
+
+		wizardAction.prepareNewServer("Liferay 7-wildfly-server");
+
+		wizardAction.finish();
+
+		viewAction.showServersView();
+
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.deleteRuntimeTryConfirm(runtimeName);
+
+		dialogAction.confirmPreferences();
+	}
+
+	@Test
+	public void serverEditorCustomLaunchSettingsChange() {
+		String serverName = "Liferay 7-wildfly-custom-launch-settings";
+
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.openNewRuntimeWizard();
+
+		wizardAction.prepareLiferay7RuntimeType();
+
+		wizardAction.next();
+
+		wizardAction.prepareLiferay7RuntimeInfo(serverName, envAction.getLiferayWildflyServerFullDir().toOSString());
+
+		wizardAction.finish();
+
+		dialogAction.confirmPreferences();
+
+		wizardAction.openNewLiferayServerWizard();
+
+		wizardAction.prepareNewServer(serverName);
+
+		wizardAction.finish();
+
+		viewAction.showServersView();
+
+		String serverStoppedLabel = serverName + "  [Stopped]";
+
+		viewAction.openServerEditor(serverStoppedLabel);
+
+		ServerEditor serverEditor = new ServerEditor(bot, serverName);
+		ServerEditor serverEditorWithLabel = new ServerEditor(bot, serverStoppedLabel);
+
+		try {
+			serverEditor.getCustomLaunchSettings().click();
+		} catch (Exception e) {
+			serverEditorWithLabel.getCustomLaunchSettings().click();
+		}
+
+		try {
+			serverEditor.getUseDeveloperMode().select();
+		} catch (Exception e) {
+			serverEditorWithLabel.getUseDeveloperMode().select();
+		}
+
+		editorAction.save();
+
+		editorAction.close();
+
+		viewAction.openServerEditor(serverStoppedLabel);
+
+		try {
+			serverEditor.getDefaultLaunchSettings().click();
+		} catch (Exception e) {
+			serverEditorWithLabel.getDefaultLaunchSettings().click();
+		}
+
+		editorAction.save();
+
+		editorAction.close();
+
+		dialogAction.openPreferencesDialog();
+
+		dialogAction.deleteRuntimeTryConfirm(serverName);
+
+		dialogAction.confirmPreferences();
 	}
 
 }

c25b95b18373d86b2e51a0742baed6ea4d7e8879
Simon Jiang <simon.jiang@liferay.com>
2018-05-25 13:51:58 +0800

    IDE-4073 Enable advanced source lookup for Tomcat servers
    tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/PortalServerLaunchConfigDelegate.java#

--- tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/PortalServerLaunchConfigDelegate.java
+++ tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/PortalServerLaunchConfigDelegate.java
@@ -19,19 +19,32 @@ import com.liferay.ide.server.core.LiferayServerCore;
 
 import java.io.File;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.sourcelookup.AbstractSourceLookupDirector;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
+import org.eclipse.jdt.internal.debug.core.JDIDebugPlugin;
+import org.eclipse.jdt.internal.launching.LaunchingPlugin;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.AbstractVMInstall;
 import org.eclipse.jdt.launching.ExecutionArguments;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMRunner;
+import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.VMRunnerConfiguration;
 import org.eclipse.wst.server.core.IRuntime;
 import org.eclipse.wst.server.core.IServer;
@@ -43,9 +56,22 @@ import org.eclipse.wst.server.core.ServerUtil;
  * @author Gregory Amerson
  * @author Charles Wu
  */
+@SuppressWarnings("restriction")
 public class PortalServerLaunchConfigDelegate extends AbstractJavaLaunchConfigurationDelegate {
 
 	public static final String ID = "com.liferay.ide.server.portal.launch";
+
+	public static final String PREF_ENABLE_ADVANCED_SOURCELOOKUP =
+		JDIDebugPlugin.getUniqueIdentifier() + ".enable_advanced_sourcelookup";
+
+	@Override
+	public ILaunch getLaunch(ILaunchConfiguration configuration, String mode) throws CoreException {
+		if (!(ILaunchManager.DEBUG_MODE.equals(mode) && _isAdvancedSourcelookupEnabled())) {
+			return null;
+		}
+
+		return super.getLaunch(configuration, mode);
+	}
 
 	@Override
 	public void launch(ILaunchConfiguration config, String mode, ILaunch launch, IProgressMonitor monitor)
@@ -74,6 +100,80 @@ public class PortalServerLaunchConfigDelegate extends AbstractJavaLaunchConfigur
 
 			_launchServer(server, config, mode, launch, monitor);
 		}
+	}
+
+	private static int _compareJavaVersions(IVMInstall vm, String ver) {
+		if (vm instanceof AbstractVMInstall) {
+			AbstractVMInstall install = (AbstractVMInstall)vm;
+
+			String vmver = install.getJavaVersion();
+
+			if (vmver == null) {
+				return -1;
+			}
+
+			// versionToJdkLevel only handles 3 char versions = 1.5, 1.6, 1.7, etc
+
+			if (vmver.length() > 3) {
+				vmver = vmver.substring(0, 3);
+			}
+
+			return Long.compare(CompilerOptions.versionToJdkLevel(vmver), CompilerOptions.versionToJdkLevel(ver));
+		}
+
+		return -1;
+	}
+
+	private static String _getJavaagentLocation() {
+		return LaunchingPlugin.getFileInPlugin(new Path("lib/javaagent-shaded.jar")).getAbsolutePath(); //$NON-NLS-1$
+	}
+
+	private static String _getJavaagentString() {
+		return "-javaagent:\"" + _getJavaagentLocation() + "\""; //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	private static boolean _isAdvancedSourcelookupEnabled() {
+		IPreferencesService preferencesService = Platform.getPreferencesService();
+
+		return preferencesService.getBoolean(
+			JDIDebugPlugin.getUniqueIdentifier(), PREF_ENABLE_ADVANCED_SOURCELOOKUP, false, null);
+	}
+
+	private String _getVMArgumentsForSourceLookUp(ILaunchConfiguration configuration, String mode)
+		throws CoreException {
+
+		if (!_isAdvancedSourcelup(mode)) {
+			return ""; //$NON-NLS-1$
+		}
+
+		if (!_isJavaagentOptionSupported(configuration)) {
+			return ""; //$NON-NLS-1$
+		}
+
+		return _getJavaagentString();
+	}
+
+	private boolean _isAdvancedSourcelup(String mode) {
+		if (ILaunchManager.DEBUG_MODE.equals(mode) && _isAdvancedSourcelookupEnabled()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _isJavaagentOptionSupported(ILaunchConfiguration configuration) {
+		try {
+			IVMInstall vm = JavaRuntime.computeVMInstall(configuration);
+
+			if (_compareJavaVersions(vm, JavaCore.VERSION_1_4) > 0) {
+				return true;
+			}
+		}
+		catch (CoreException ce) {
+			LaunchingPlugin.log(ce);
+		}
+
+		return false;
 	}
 
 	private void _launchServer(
@@ -113,7 +213,13 @@ public class PortalServerLaunchConfigDelegate extends AbstractJavaLaunchConfigur
 		VMRunnerConfiguration runConfig = new VMRunnerConfiguration(classToLaunch, classpath);
 
 		runConfig.setProgramArguments(execArgs.getProgramArgumentsArray());
-		runConfig.setVMArguments(execArgs.getVMArgumentsArray());
+
+		List<String> vmArguments = new ArrayList<>();
+
+		Collections.addAll(vmArguments, DebugPlugin.parseArguments(_getVMArgumentsForSourceLookUp(config, mode)));
+		Collections.addAll(vmArguments, execArgs.getVMArgumentsArray());
+		runConfig.setVMArguments(vmArguments.toArray(new String[vmArguments.size()]));
+
 		runConfig.setWorkingDirectory(workingDirPath);
 		runConfig.setEnvironment(envp);
 		runConfig.setVMSpecificAttributesMap(vmAttributesMap);

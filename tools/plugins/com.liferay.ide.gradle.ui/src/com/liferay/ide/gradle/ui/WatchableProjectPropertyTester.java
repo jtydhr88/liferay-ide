64f9d97a3d9b2ae197f34587d9b71c8283bbce19
Haoyi Sun <haoyi.sun@liferay.com>
2018-08-10 18:09:22 +0800

    IDE-4124 don't appear watch popup that if module don't support watch task

--- tools/plugins/com.liferay.ide.gradle.ui/src/com/liferay/ide/gradle/ui/WatchableProjectPropertyTester.java
+++ tools/plugins/com.liferay.ide.gradle.ui/src/com/liferay/ide/gradle/ui/WatchableProjectPropertyTester.java
@@ -14,8 +14,11 @@
 
 package com.liferay.ide.gradle.ui;
 
+import com.liferay.ide.core.util.FileUtil;
 import com.liferay.ide.gradle.core.GradleUtil;
 import com.liferay.ide.project.core.util.LiferayWorkspaceUtil;
+
+import java.io.File;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
@@ -35,6 +38,16 @@ public class WatchableProjectPropertyTester extends PropertyTester {
 			IFile buildFile = project.getFile("build.gradle");
 
 			if (LiferayWorkspaceUtil.inLiferayWorkspace(project) || LiferayWorkspaceUtil.isValidWorkspace(project)) {
+				File projectFile = FileUtil.getFile(project);
+
+				File parentFile = projectFile.getParentFile();
+
+				String parentName = parentFile.getName();
+
+				if (!parentName.equals("modules")) {
+					return false;
+				}
+
 				IProject workspaceProject = LiferayWorkspaceUtil.getWorkspaceProject();
 
 				buildFile = workspaceProject.getFile("settings.gradle");

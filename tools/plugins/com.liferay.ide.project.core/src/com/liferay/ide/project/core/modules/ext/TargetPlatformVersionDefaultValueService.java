125b3887192761f05f7968c94a89059f83f42bc0
Charles Wu <charles.wu@liferay.com>
2018-10-01 20:23:13 +0800

    IDE-4135 new module ext wizard

--- tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/modules/ext/TargetPlatformVersionDefaultValueService.java
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/modules/ext/TargetPlatformVersionDefaultValueService.java
@@ -12,29 +12,27 @@
  * details.
  */
 
-package com.liferay.ide.core;
+package com.liferay.ide.project.core.modules.ext;
 
-import java.util.List;
-import java.util.Set;
+import com.liferay.ide.core.IWorkspaceProject;
+import com.liferay.ide.project.core.util.LiferayWorkspaceUtil;
 
-import org.eclipse.core.resources.IProject;
+import org.eclipse.sapphire.DefaultValueService;
 
 /**
- * @author Gregory Amerson
- * @author Simon Jiang
+ * @author Charles Wu
  */
-public interface IWorkspaceProject extends ILiferayProject {
+public class TargetPlatformVersionDefaultValueService extends DefaultValueService {
 
-	public Set<IProject> getChildProjects();
+	@Override
+	protected String compute() {
+		IWorkspaceProject gradleWorkspaceProject = LiferayWorkspaceUtil.getGradleWorkspaceProject();
 
-	public List<Artifact> getTargetPlatformArtifacts(boolean reload);
+		if (gradleWorkspaceProject != null) {
+			return gradleWorkspaceProject.getTargetPlatformVersion();
+		}
 
-	public String getTargetPlatformVersion();
-
-	public boolean isWatchable();
-
-	public void watch(Set<IProject> childProjects);
-
-	public Set<IProject> watching();
+		return null;
+	}
 
 }

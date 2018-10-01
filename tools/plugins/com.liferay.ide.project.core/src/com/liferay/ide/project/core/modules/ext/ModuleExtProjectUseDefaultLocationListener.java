b48d8d602192d1c9048c28f7a7759c3505a9b021
Charles Wu <charles.wu@liferay.com>
2018-10-01 20:23:13 +0800

    IDE-4135 New module ext wizard UI

---
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/modules/ext/ModuleExtProjectUseDefaultLocationListener.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.project.core.modules.ext;
+
+import com.liferay.ide.core.util.SapphireUtil;
+
+import org.eclipse.sapphire.PropertyContentEvent;
+
+/**
+ * @author Charles Wu
+ */
+public class ModuleExtProjectUseDefaultLocationListener extends ModuleExtProjectNameListener {
+
+	@Override
+	protected void handleTypedEvent(PropertyContentEvent event) {
+		final NewModuleExtOp op = op(event);
+
+		if (SapphireUtil.getContent(op.getUseDefaultLocation())) {
+			super.handleTypedEvent(event);
+		}
+	}
+
+}

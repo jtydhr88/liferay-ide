f51b2739f71df02e1d9b45cb0548f2e39d3d4111
Simon Jiang <simon.jiang@liferay.com>
2019-02-19 16:25:36 +0800

    IDE-4538 add upgrade category in new upgrade plan wizard

---
+++ tools/plugins/com.liferay.ide.upgrade.plan.core/src/com/liferay/ide/upgrade/plan/core/UpgradeCategoryValueLabelService.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.upgrade.plan.core;
+
+import com.liferay.ide.upgrade.plan.core.internal.NewUpgradePlanOpMethods;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.eclipse.sapphire.services.ValueLabelService;
+
+/**
+ * @author Simon Jiang
+ */
+public final class UpgradeCategoryValueLabelService extends ValueLabelService {
+
+	@Override
+	public String provide(final String value) {
+		if (value != null) {
+			List<UpgradeTaskCategory> upgradeCategories = NewUpgradePlanOpMethods.getUpgradeCategories();
+
+			Stream<UpgradeTaskCategory> upgradeCategorieStream = upgradeCategories.stream();
+
+			UpgradeTaskCategory upgradeTaskCategory = upgradeCategorieStream.filter(
+				upgradeCategory -> value.equalsIgnoreCase(upgradeCategory.getId())
+			).findFirst(
+			).get();
+
+			return upgradeTaskCategory.getTitle();
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void init() {
+		if (_upgradeCategories == null) {
+			_upgradeCategories = NewUpgradePlanOpMethods.getUpgradeCategories();
+		}
+	}
+
+	private static List<UpgradeTaskCategory> _upgradeCategories;
+
+}

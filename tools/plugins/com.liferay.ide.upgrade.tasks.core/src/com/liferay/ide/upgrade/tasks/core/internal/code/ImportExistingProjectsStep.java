8845651c74b32976c287c7bb10cf3cd639bee42a
Lily Li <lily.li@liferay.com>
2019-03-04 14:14:39 +0800

    IDE-3972 add descriptions for setup development environment

--- tools/plugins/com.liferay.ide.upgrade.tasks.core/src/com/liferay/ide/upgrade/tasks/core/internal/code/ImportExistingProjectsStep.java
+++ tools/plugins/com.liferay.ide.upgrade.tasks.core/src/com/liferay/ide/upgrade/tasks/core/internal/code/ImportExistingProjectsStep.java
@@ -32,7 +32,8 @@ import org.osgi.service.component.annotations.ServiceScope;
 @Component(
 	property = {
 		"id=import_existing_projects", "requirement=required", "order=1", "taskId=setup_development_environment",
-		"title=Import Existing Projects"
+		"title=Import Existing Projects",
+		"description=Import Liferay Workspace that need to be upgraded into Project Explorer."
 	},
 	scope = ServiceScope.PROTOTYPE, service = UpgradeTaskStep.class
 )

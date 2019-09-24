ec14f2ca9ac54482f02182c35a3bd943a528f37e
Terry Jia <terry.jia@liferay.com>
2019-09-24 16:43:19 +0800

    IDE-4684 LSP support

---
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/languageserver/LanguageServerConnectionProvider.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.project.ui.languageserver;
+
+import com.liferay.ide.core.util.CoreUtil;
+import com.liferay.ide.project.core.modules.BladeCLI;
+import com.liferay.ide.project.core.modules.BladeCLIException;
+
+import java.util.Arrays;
+
+import org.eclipse.lsp4e.server.ProcessOverSocketStreamConnectionProvider;
+import org.eclipse.wst.server.core.util.SocketUtil;
+
+/**
+ * @author Terry Jia
+ */
+public class LanguageServerConnectionProvider extends ProcessOverSocketStreamConnectionProvider {
+
+	public LanguageServerConnectionProvider() throws BladeCLIException {
+		super(
+			Arrays.asList(
+				"java", "-jar", BladeCLI.getBladeCLIPathString(), "languageServer", "-p", String.valueOf(_port)),
+			CoreUtil.getWorkspaceRootLocationString(), _port);
+	}
+
+	private static int _port = SocketUtil.findUnusedPort(10000, 60000);
+
+}

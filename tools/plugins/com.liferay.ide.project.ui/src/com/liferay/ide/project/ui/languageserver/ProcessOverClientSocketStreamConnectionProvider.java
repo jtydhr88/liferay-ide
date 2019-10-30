719815ff3e8c2c6bc639a48edd2cd0795b8dc745
Seiphon <871358932@qq.com>
2019-10-30 14:37:02 +0800

    IDE-4684 Use user home and do sf

--- tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/languageserver/ProcessOverClientSocketStreamConnectionProvider.java
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/languageserver/ProcessOverClientSocketStreamConnectionProvider.java
@@ -1,42 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.ide.project.ui.languageserver;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import java.net.Socket;
+
 import java.util.List;
 
 import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
 
+/**
+ * @author Terry Jia
+ */
 public class ProcessOverClientSocketStreamConnectionProvider extends ProcessStreamConnectionProvider {
-
-	private int port;
-	private Socket _socket;
-	private InputStream _inputStream;
-	private OutputStream _outputStream;
 
 	public ProcessOverClientSocketStreamConnectionProvider(List<String> commands, int port) {
 		super(commands);
-		this.port = port;
+
+		_port = port;
 	}
 
 	public ProcessOverClientSocketStreamConnectionProvider(List<String> commands, String workingDir, int port) {
 		super(commands, workingDir);
-		this.port = port;
-	}
 
-	@Override
-	public void start() throws IOException {
-		super.start();
-
-		if (_socket != null) {
-			return;
-		}
-
-		_socket = new Socket("localhost", port);
-
-		_inputStream = _socket.getInputStream();
-		_outputStream = _socket.getOutputStream();
+		_port = port;
 	}
 
 	@Override
@@ -50,17 +52,37 @@ public class ProcessOverClientSocketStreamConnectionProvider extends ProcessStre
 	}
 
 	@Override
+	public void start() throws IOException {
+		super.start();
+
+		if (_socket != null) {
+			return;
+		}
+
+		_socket = new Socket("localhost", _port);
+
+		_inputStream = _socket.getInputStream();
+		_outputStream = _socket.getOutputStream();
+	}
+
+	@Override
 	public void stop() {
 		if (_socket != null) {
 			try {
 				_socket.close();
 			}
-			catch (IOException e) {
+			catch (IOException ioe) {
 			}
-			
+
 			_socket = null;
 		}
 
 		super.stop();
 	}
+
+	private InputStream _inputStream;
+	private OutputStream _outputStream;
+	private int _port;
+	private Socket _socket;
+
 }
